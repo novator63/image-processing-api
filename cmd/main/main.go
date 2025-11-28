2cede710614a2cccce9fa99f1f587f6df7a3da4a
Кирилл Пачкалов <pachk.kir@gmail.com>
2025-11-28 14:43:17 +0400

    feat: server init, middleware configuration, test server starting

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 	"program/internal/config"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Addr:        cfg.HTTPServer.Addres,
+		Handler:     router,
+		ReadTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout: cfg.HTTPServer.IdleTimeout,
+	}
+
+	router.Use(middleware.Logger)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Timeout(cfg.Timeout))
+
+	router.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		resp := map[string]string{"message":"pong"}
+		json.NewEncoder(w).Encode(resp)
+	})
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("starting server error: %s", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.4
 
 require (
 	github.com/BurntSushi/toml v1.5.0 // indirect
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
